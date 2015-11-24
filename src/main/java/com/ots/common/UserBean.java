cfef9faf107a275b5a21fac837da282c24a69709
Madhuri <Madhuri@10.21.43.225>
2015-11-23 19:51:03 -0600

    userbean

---
+++ src/main/java/com/ots/common/UserBean.java
@@ -0,0 +1,205 @@
+package com.ots.common;
+
+/**
+ * Bean for User Name
+ * 
+ * @author Madhuri
+ *
+ */
+public class UserBean {
+
+	private String firstName;
+	private String lastName;
+	private String apartmentNumber;
+	private String street;
+	private String city;
+	private String state;
+	private int zipcode;
+	private int phoneNumber;
+	private int cellPhoneNumber;
+	private String emailId;
+	private String password;
+
+	private String userType;
+
+	/**
+	 * @return the userType
+	 */
+	public String getUserType() {
+		return userType;
+	}
+
+	/**
+	 * @param userType
+	 *            the userType to set
+	 */
+	public void setUserType(String userType) {
+		this.userType = userType;
+	}
+
+	/**
+	 * @return the firstName
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+
+	/**
+	 * @param firstName
+	 *            the firstName to set
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	/**
+	 * @return the lastName
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+
+	/**
+	 * @param lastName
+	 *            the lastName to set
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	/**
+	 * @return the apartmentNumber
+	 */
+	public String getApartmentNumber() {
+		return apartmentNumber;
+	}
+
+	/**
+	 * @param apartmentNumber
+	 *            the apartmentNumber to set
+	 */
+	public void setApartmentNumber(String apartmentNumber) {
+		this.apartmentNumber = apartmentNumber;
+	}
+
+	/**
+	 * @return the street
+	 */
+	public String getStreet() {
+		return street;
+	}
+
+	/**
+	 * @param street
+	 *            the street to set
+	 */
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	/**
+	 * @return the city
+	 */
+	public String getCity() {
+		return city;
+	}
+
+	/**
+	 * @param city
+	 *            the city to set
+	 */
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	/**
+	 * @return the state
+	 */
+	public String getState() {
+		return state;
+	}
+
+	/**
+	 * @param state
+	 *            the state to set
+	 */
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	/**
+	 * @return the zipcode
+	 */
+	public int getZipcode() {
+		return zipcode;
+	}
+
+	/**
+	 * @param zipcode
+	 *            the zipcode to set
+	 */
+	public void setZipcode(int zipcode) {
+		this.zipcode = zipcode;
+	}
+
+	/**
+	 * @return the phoneNumber
+	 */
+	public int getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	/**
+	 * @param phoneNumber
+	 *            the phoneNumber to set
+	 */
+	public void setPhoneNumber(int phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	/**
+	 * @return the cellPhoneNumber
+	 */
+	public int getCellPhoneNumber() {
+		return cellPhoneNumber;
+	}
+
+	/**
+	 * @param cellPhoneNumber
+	 *            the cellPhoneNumber to set
+	 */
+	public void setCellPhoneNumber(int cellPhoneNumber) {
+		this.cellPhoneNumber = cellPhoneNumber;
+	}
+
+	/**
+	 * @return the emailId
+	 */
+	public String getEmailId() {
+		return emailId;
+	}
+
+	/**
+	 * @param emailId
+	 *            the emailId to set
+	 */
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * @param password
+	 *            the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
