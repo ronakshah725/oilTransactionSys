3cc1f7c3c794d48e4ae4e3ed675a6fb92a38e29d
Ronak Shah <Ronak Shah@192.168.130.130>
2015-11-25 14:04:33 -0600

    DAO

---
+++ src/main/java/com/ots/dao/OrderDaoImpl.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.ots.dao;
+
+/**
+ * @author kanchan
+ *
+ */
+public class OrderDaoImpl {
+
+	 	public static final String QUERY_INSERT_TASK = "INSERT INTO orders(id,type,trans_fee,quantity,commission_fees,commission_type,total_amt,oil_adjusted_quantity,date_placed ) VALUES (?,?,?,?,?,?,?,?,?)";
+	 	public static final String SELECT_ORDER_BY_USER_ID = "SELECT * FROM orders where order_id IN (SELECT order_id FROM places where client_id=?)";
+	 	public static final String UPDATE_ORDER = "UPDATE orders SET payment_id = ? WHERE order_id in (?)";
+ 
+
+
+}
+
