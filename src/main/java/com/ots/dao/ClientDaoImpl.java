2b90d61047003415fd11067a6bbe049957c02a6c
Ronak Shah <Ronak Shah@192.168.130.130>
2015-11-25 14:21:51 -0600

    clientdao

--- src/main/java/com/ots/dao/ClientDaoImpl.java
+++ src/main/java/com/ots/dao/ClientDaoImpl.java
@@ -3,6 +3,20 @@
  */
 package com.ots.dao;
 
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementSetter;
+
+import com.ots.common.ClientBean;
+import com.ots.common.UserBean;
+import com.ots.rowmapper.ClientRowMapper;
+import com.ots.rowmapper.UserRowMapper;
+
 /**
  * @author kanchan
  *
@@ -13,6 +27,39 @@ public class ClientDaoImpl {
 
 	 	public static final String QUERY_INSERT_TASK = "INSERT INTO client(client_id) VALUES (?)";
 
+		private JdbcTemplate adminJdbcConnectionTemplate;
+		private JdbcTemplate traderJdbcConnectionTemplate;
+		private JdbcTemplate clientJdbcConnectionTemplate;
+
+		@Autowired
+		public void setDataSource(DataSource adminDataSource, DataSource traderDataSource, DataSource clientDataSource) {
+			this.adminJdbcConnectionTemplate = new JdbcTemplate(adminDataSource);
+			this.traderJdbcConnectionTemplate = new JdbcTemplate(traderDataSource);
+			this.clientJdbcConnectionTemplate = new JdbcTemplate(clientDataSource);
+		}
+		
+		
+		public ClientBean getClientDetails(final String clientID) {
+
+			List<ClientBean> clientBean = adminJdbcConnectionTemplate.query(SELECT_CLIENT_BY_CLIENT_ID,
+					new PreparedStatementSetter() {
+						public void setValues(java.sql.PreparedStatement ps) throws SQLException {
+							ps.setString(1, clientID);
+						
+						}
+					}, new ClientRowMapper());
+			try {
+				adminJdbcConnectionTemplate.getDataSource().getConnection().setAutoCommit(false); // doesnt work! -set it in datasource definition in ots-servlet.xml
+			//	adminJdbcConnectionTemplate.getDataSource().getConnection().commit();
+			} catch (SQLException sqle) {
+				sqle.printStackTrace();
+			}
+			if (/* CollectionUtils.isNotEmpty( */clientBean != null && clientBean.size() != 0) {
+				return clientBean.get(0);
+			} else {
+				return null;
+			}
+		}
  
 
 
