063fd90776cfd35d4503969edeaecd8b6325aabd
Ronak Shah <ronakshah725@gmail.com>
2016-03-07 12:37:35 -0600

    Moved contents of docs folder

--- docs/ots_setup_users.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE ots CHARACTER SET utf8 COLLATE utf8_general_ci;
-
-use ots;
-
-CREATE USER 'admin'@'localhost' IDENTIFIED BY 'admin123';
-CREATE USER 'client'@'localhost' IDENTIFIED BY 'client@123';
-CREATE USER 'trader'@'localhost' IDENTIFIED BY 'trader@123';
-GRANT ALL PRIVILEGES ON ots.* TO 'admin'@'localhost'  WITH GRANT OPTION;
